11a61480238c16db13bc1edce2df49b861433097
Premasagar Rose <p@dharmafly.com>
2010-08-28 12:06:56 -0400

    dir restructure

--- phi/phi-v1.js
+++ phi/phi-v1.js
@@ -1,1 +1,151 @@
-var W=this,b=Math,d=.618,f=1/d,i=255,j=b.ceil,k=b.random,l=f,m=1,n=m,o=400,p=o,t=100,u=document,v=u.getElementById("c"),w=v.getContext("2d"),x=v.width=W.innerWidth,y=v.height=W.innerHeight,z,B,C=[],D=k(),E=D>=.5?0:D<=.3?1:2,F="rgba(",G="stroke",H=G+"Style",I="beginPath",M="closePath";function N(){function g(q){return C[q]=j(e&&q==E||!e&&q!=E?A*i:i-(C[q]||i)*a)}var a=k()+d/2,e=k()>.5,A,h,r,s,J,K,L;if(z>x-t/2)m=-m;if(B>y-t/2)n=-n;if(z<=o){z=o;m=1}if(B<=p){B=p;n=1}z=j(m>0?z*l:z/l*a);B=j(n>0?B*l:B/l*a);A=z/x;J=g(0);K=g(1);L=g(2);h=j(A*t*a);r=j(z-o-h/2);s=j(B-p-h/2);if(r>x||s>y)return[j(x/f)*a,j(y/f)*a];w[I]();w.arc(r,s,h,0,b.PI*2,0);w[M]();h=F+J+","+K+","+L+",";w[H]=h+d*a+")";w.fillStyle=h+d*d*d*a+")";w[G]();w.fill();return[r,s]}u.body.style.cssText="margin:0;background-color:#000;overflow:hidden";z=j(x*k());B=j(y*k());W.setInterval(function(){for(var g=[],a=0,e;a<10;a++)g[a]=N();w[I]();e=g[0];w.moveTo(e[0],e[1]);for(a=10;a;a--){e=g[a-1];w.lineTo(e[0],e[1])}w[M]();w[H]=F+"0,0,0,.1)";w[G]()},40)
+var win = this,
+
+    // Maths
+    M = Math,
+    phi = .618,
+    PHI = 1 / phi,
+    RGBMAX = 255,
+    ceil = M.ceil,
+    random = M.random,
+    
+    // Modifiers
+    factor = PHI, //1 + PHI / (PHI * PHI),
+    dirX = 1,
+    dirY = dirX,
+    minX = 400,
+    minY = minX,
+    maxRadius = 100,
+    
+    // Render cycles
+    intervalRef,
+    frequency = 1000 / 25, // 1 second, per x frames
+    steps = 10,
+    
+    // Document
+    doc = document,
+    
+    // Canvas
+    canvas = doc.getElementById('c'),
+    ctx = canvas.getContext('2d'),
+    w = canvas.width = win.innerWidth,
+    h = canvas.height = win.innerHeight,
+    x, y,
+    
+    // Color
+    colors = [],
+    toneSeed = random(),
+    tone = toneSeed >= .5 ?
+        0 : // r
+        toneSeed <= .3 ?
+        1 : // g
+        2,  // b
+    
+    // String lookups
+    rgba = 'rgba(',
+    stroke = 'stroke',
+    strokeStyle = stroke + 'Style',
+    beginPath = 'beginPath',
+    closePath = 'closePath',
+    click = 'onclick';
+    
+function draw(){
+    var drift = random() + phi / 2,
+        intenseColor = random() > .5, // in this loop, should the color be intense or not?
+        intensity, radius, posX, posY, rbgStr, r, g, b;
+    
+    function colorComponent(color){
+        return colors[color] = ceil(
+            (intenseColor && color == tone) || (!intenseColor && color != tone) ? // if this color is the general tone
+                intensity * RGBMAX : // then change it according to the intensity
+                RGBMAX - (colors[color] || RGBMAX) * drift // otherwise, add some random component
+        );
+    }
+    
+    // direction & coords
+    if (x > w - maxRadius / 2){
+        dirX = -dirX;
+    }
+    if (y > h - maxRadius / 2){
+        dirY = -dirY;
+    }
+    if (x <= minX){
+        x = minX;
+        dirX = 1;
+    }
+    if (y <= minY){
+        y = minY;
+        dirY = 1;
+    }
+    x = ceil(dirX > 0 ? x * factor : (x / factor) * drift);
+    y = ceil(dirY > 0 ? y * factor : (y / factor) * drift);
+    
+    // intensity
+    intensity = x / w;
+    
+    // color
+    r = colorComponent(0);
+    g = colorComponent(1);
+    b = colorComponent(2);
+    
+    // radius
+    radius = ceil(intensity * maxRadius * drift);
+    
+    // positions
+    posX = ceil(x - minX - radius / 2); // TODO store var
+    posY = ceil(y - minY - radius / 2);
+    
+    if (posX > w || posY > h){
+        return [ceil(w / PHI) * drift, ceil(h / PHI) * drift];
+    }
+    
+    // paths
+    ctx[beginPath]();
+    ctx.arc(posX, posY, radius, 0, M.PI * 2, 0);
+    ctx[closePath]();
+    
+    // styles
+    rbgStr = rgba + r + ',' + g + ',' + b + ',';
+    ctx[strokeStyle] = rbgStr + phi * drift +')';
+    ctx.fillStyle = rbgStr + (phi * phi * phi * drift) +')';
+    
+    // draw
+    ctx[stroke]();
+    ctx.fill();
+    
+    return [posX, posY];
+}
+
+function render(){
+    // draw circles, in a number of steps
+    for (var coords = [], i = 0, xy; i < steps; i++){
+        coords[i] = draw();
+    }
+    
+    // draw connecting lines
+    ctx[beginPath]();
+    xy = coords[0];
+    ctx.moveTo(xy[0], xy[1]);
+    
+    for (i = steps; i; i--){
+        xy = coords[i-1];
+        ctx.lineTo(xy[0], xy[1]);
+    }
+    ctx[closePath]();
+    ctx[strokeStyle] = rgba + '0,0,0,.1)';
+    ctx[stroke]();
+}
+
+doc.body.style.cssText = 'margin:0;background-color:#000;overflow:hidden';
+
+x = ceil(w * random());
+y = ceil(h * random());
+
+// toggle animation on any mouse click or key press
+canvas[click] = doc.onkeydown = function(event){
+    intervalRef = intervalRef ?
+        win.clearInterval(intervalRef) :    // clearInterval and set intervalRef to undefined
+        win.setInterval(render, frequency);  // setInterval and create reference to it
+};
+
+// start animation
+canvas[click]();
